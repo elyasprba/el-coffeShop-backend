8613c3aa0b6c319e5468bc36b0379e2569a98a98
elyasprba <elyaspoerba21@gmail.com>
2022-04-27 14:37:02 +0700

    update validate

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,15 +1,21 @@
-const validateCreatUsers = (req, res, next) => {
-   // email, password, phone_number
-   const query = req.query;
-   const validQuery = Object.keys(query).filter((key) => key === 'email' || key === 'password' || key === 'phone_number');
-   if (validQuery.length < 3) {
-      return res.status(400).json({
-         err: 'Query harus berisikan email, password, phone_number',
-      });
-   }
-   next();
-};
+const { check, validationResult } = require('express-validator');
+
+const rules = [check('email').isEmail().bail().notEmpty().withMessage('The email is required'), check('password').exists().notEmpty().withMessage('The password is required'), check('phone_number').toInt().notEmpty().withMessage('The number phone is required')];
+
+const validateCreateUsers = [
+   rules,
+   (req, res, next) => {
+      const error = validationResult(req);
+
+      if (!error.isEmpty()) {
+         return res.status(422).json({
+            error: error.array(),
+         });
+      }
+      next();
+   },
+];
 
 module.exports = {
-   validateCreatUsers,
+   validateCreateUsers,
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,7 @@ const Router = require('express').Router();
 const userController = require('../controllers/users');
 const validate = require('../middlewares/validate');
 
-Router.post('/', validate.validateCreatUsers, userController.postUsersControllers);
+Router.post('/', validate.validateCreateUsers, userController.postUsersControllers);
 Router.get('/all', userController.getUsersControllers);
 Router.patch('/:id', userController.patchUsersControllers);
 Router.delete('/:id', userController.deleteUsersControllers);
