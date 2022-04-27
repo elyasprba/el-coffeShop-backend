32d4a08eae616ad905a025dc31dafb51d0d6aa4e
elyasprba <elyaspoerba21@gmail.com>
2022-04-28 00:23:58 +0700

    update

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -16,18 +16,6 @@ const validateCreateUsers = [
    },
 ];
 
-const validateUpdateData = (req, res, next) => {
-   const { body } = req;
-   const validBody = Object.keys(body).filter((key) => key === 'password' || key === 'phone_number' || key === 'pict' || key === 'display_name' || key === 'first_name' || key === 'last_name' || key === 'birthday_date' || key === 'address' || key === 'gender');
-   if (validBody.length < 9) {
-      return res.status(400).json({
-         err: 'Body harus berisikan data lengkap',
-      });
-   }
-   next();
-};
-
 module.exports = {
    validateCreateUsers,
-   validateUpdateData,
 };
