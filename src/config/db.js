ee9fea80de1688202908e04d0dd3e50fe6c3c271
elyasprba <elyaspoerba21@gmail.com>
2022-06-20 23:15:42 +0700

    update config db

--- src/config/db.js
+++ src/config/db.js
@@ -2,16 +2,16 @@ const psql = require('pg');
 const { Pool } = psql;
 
 const db = new Pool({
-   user: process.env.DB_USER,
-   host: process.env.DB_HOST,
-   database: process.env.DB_DATABASE,
-   password: process.env.DB_PASSWORD,
-   port: process.env.DB_PORT,
+   // user: process.env.DB_USER,
+   // host: process.env.DB_HOST,
+   // database: process.env.DB_DATABASE,
+   // password: process.env.DB_PASSWORD,
+   // port: process.env.DB_PORT,
 
-   // connectionString: process.env.DATABASE_URL,
-   // ssl: {
-   //    rejectUnauthorized: false,
-   // },
+   connectionString: process.env.DATABASE_URL,
+   ssl: {
+      rejectUnauthorized: false,
+   },
 });
 
 module.exports = db;
